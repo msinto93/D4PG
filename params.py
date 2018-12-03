366021bafcbc73685dfb0b1310101ae6fa97cf48
Mark Sinton <msinto93@gmail.com>
2018-12-03 21:00:13 +0000

    Update params.py

--- params.py
+++ params.py
@@ -43,7 +43,7 @@ class train_params:
     FINAL_LAYER_INIT = 0.003        # Initialise networks' final layer weights in range +/-final_layer_init
     NUM_ATOMS = 51                  # Number of atoms in output layer of distributional critic
     V_MIN = -20.0                   # Lower bound of critic value output distribution
-    V_MAX = 20.0                    # Upper bound of critic value output distribution (V_min and V_max should be chosen based on the range of normalised reward values in the chosen env)
+    V_MAX = 0.0                    # Upper bound of critic value output distribution (V_min and V_max should be chosen based on the range of normalised reward values in the chosen env)
     TAU = 0.001                     # Parameter for soft target network updates
     USE_BATCH_NORM = False          # Whether or not to use batch normalisation in the networks
   
